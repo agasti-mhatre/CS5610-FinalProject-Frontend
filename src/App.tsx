b3c9cfe89c1b87305047a603946e91635c5d8b12
agasti-mhatre <agastimhatre6@gmail.com>
2024-10-30 10:33:29 -0400

    Up to 3.4

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import './App.css';
 import Labs from "./Labs";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Kanbas from './Kanbas';
+import store from "./Kanbas/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <HashRouter>
+      <Provider store = {store}>
         <div>
           <Routes>
             <Route path="/" element={<Navigate to="Kanbas"/>} />
@@ -14,6 +17,7 @@ function App() {
             <Route path="/Kanbas/*" element={<Kanbas />} />
           </Routes>
         </div>
+      </Provider>
     </HashRouter>
   );
 }
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -4,31 +4,25 @@ import ModuleControlButtons from "./ModuleControlButtons";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import { useParams } from "react-router";
-import * as db from "../../Database";
+import { addModule, editModule, updateModule, deleteModule }
+  from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function Modules() {
-    
-  const [modules, setModules] = useState<any[]>(db.modules);
+
   const { cid } = useParams();
   const [moduleName, setModuleName] = useState("");
-  const addModule = () => {
-    setModules([ ...modules, { _id: new Date().getTime().toString(),
-                                     name: moduleName, course: cid, lessons: [] } ]);
-    setModuleName("");
-  };
-  const deleteModule = (moduleId: string) => {
-    setModules(modules.filter((m) => m._id !== moduleId));
-  };
-  const editModule = (moduleId: string) => {
-    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
-  };
-  const updateModule = (module: any) => {
-    setModules(modules.map((m) => (m._id === module._id ? module : m)));
-  };
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const dispatch = useDispatch();
 
   return (
       <div>
-        <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule} /><br /><br /><br /><br />
+        <ModulesControls setModuleName={setModuleName} moduleName={moduleName} 
+        addModule={ () => {
+          dispatch(addModule({name: moduleName, course: cid}));
+          setModuleName("");
+        }} />
+        <br /><br /><br /><br />
         <ul id="wd-modules" className="list-group rounded-0">
         {modules
           .filter((module: any) => module.course === cid)
@@ -39,15 +33,29 @@ export default function Modules() {
                 {!module.editing && module.name}
                 { module.editing && (
                   <input className="form-control w-50 d-inline-block"
-                        onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                        onChange={(e) => 
+                          dispatch(updateModule({ ...module, name: e.target.value }))
+                        }
                         onKeyDown={(e) => {
                           if (e.key === "Enter") {
-                            updateModule({ ...module, editing: false });
+                            dispatch(updateModule({ ...module, editing: false }));
                           }
                         }}
                         defaultValue={module.name}/>
                 )}
-                <ModuleControlButtons moduleId={module._id} deleteModule={deleteModule} editModule={editModule}/>
+                <ModuleControlButtons moduleId={module._id} deleteModule=
+                {
+                  (moduleId) => 
+                    {
+                      dispatch(deleteModule(moduleId))
+                    }
+                } 
+                editModule={
+                  (moduleId) =>
+                  {
+                    dispatch(editModule(moduleId))
+                  }
+                }/>
               </div>
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0">
