496186baf7ec43144722faa55e1362e5c60103d9
agasti-mhatre <agastimhatre6@gmail.com>
2024-11-27 23:51:50 -0500

    Up to 5.4

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,12 +6,29 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
+import { useEffect, useState } from "react";
+import * as coursesClient from "./client";
 
 export default function Courses({ courses }: { courses: any[]; }) {
 
     const { cid } = useParams();
     const course = courses.find((course) => course._id === cid);
     const { pathname } = useLocation();
+
+    const [users, setUser] = useState<any[]>([]);
+
+    const getUsers = async () => {
+
+      if(cid){
+        
+        const tempUsers = await coursesClient.findUsersForCourse(cid);
+        setUser(tempUsers);
+      }
+    }
+
+    useEffect(() => {
+      getUsers();
+    }, []);
 
     return (
       <div id="wd-courses">
@@ -33,7 +50,7 @@ export default function Courses({ courses }: { courses: any[]; }) {
                 <Route path="Modules" element={<Modules />} />
                 <Route path="Assignments" element={<Assignments />} />
                 <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-                <Route path="People" element={<PeopleTable />} />
+                <Route path="People" element={<PeopleTable users={users}/>} />
               </Routes>
             </div>
           </div>
